00c828de0173680eeff80d7a05bd249871d54331
Guilherme Rocha <guirocha1938@gmail.com>
2022-06-01 22:05:26 -0300

    Created Movement Animation

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -10,6 +10,8 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb2D;
     private Animator myAnimator;
 
+    private bool facingRight = true;
+
     //variable to play with
     public float speed = 2.0f;
     public float horizMovement;
@@ -33,5 +35,19 @@ public class PlayerMovement : MonoBehaviour
     {
         //move the character left and right
         rb2D.velocity = new Vector2(horizMovement*speed, rb2D.velocity.y);
+        myAnimator.SetFloat("speed", Mathf.Abs(horizMovement));
+        Flip(horizMovement);
+    }
+
+    //flipping function
+    private void Flip (float horizontal)
+    {
+        if (horizontal < 0 && facingRight || horizontal > 0 && !facingRight)
+        {
+            facingRight = !facingRight;
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
     }
 }
